8cb13934f0fca57963378e2a1053a0f3974934f3
Harry San Juan <harrycsanjuan@gmail.com>
2023-11-08 22:38:14 +0000

    Completed Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+
+function superbowlWin(array) {
+   let winningYear = array.find(data => data.result === "W");
+   if (winningYear) {
+    return winningYear.year;
+   } else {
+    return undefined;
+   }
+}
+
